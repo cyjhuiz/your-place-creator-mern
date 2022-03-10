e13936548532893275bd7858a9bd984fa22e7f57
Clarence Yeo <69667882+CodeOnNoScope@users.noreply.github.com>
2022-03-10 21:22:27 +0800

    entire project as developed previously

---
+++ public/precache-manifest.3f96e774602482a06fb68c546621b53a.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a62d8e826b2f12b0e7d9c0ccba9d404e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ab6de1ad43ed5a529db",
+    "url": "/static/css/0.0fdfc447.chunk.css"
+  },
+  {
+    "revision": "485d9c431c90c2d22500",
+    "url": "/static/css/4.7fff5e68.chunk.css"
+  },
+  {
+    "revision": "eda107ff18bb522a549a",
+    "url": "/static/css/5.720f707a.chunk.css"
+  },
+  {
+    "revision": "ebec6f5e9ed0ce98f5e4",
+    "url": "/static/css/6.284b6b37.chunk.css"
+  },
+  {
+    "revision": "af488a48f0afe042268a",
+    "url": "/static/css/7.02e69444.chunk.css"
+  },
+  {
+    "revision": "d6ba8d2639611fbd0b95",
+    "url": "/static/css/8.c757136f.chunk.css"
+  },
+  {
+    "revision": "02c604ee35796fa4eb23",
+    "url": "/static/css/main.e1392748.chunk.css"
+  },
+  {
+    "revision": "5ab6de1ad43ed5a529db",
+    "url": "/static/js/0.06df4f99.chunk.js"
+  },
+  {
+    "revision": "e069684e8e0a6e2b447c",
+    "url": "/static/js/3.f08e994c.chunk.js"
+  },
+  {
+    "revision": "485d9c431c90c2d22500",
+    "url": "/static/js/4.290c5b03.chunk.js"
+  },
+  {
+    "revision": "eda107ff18bb522a549a",
+    "url": "/static/js/5.a395a59c.chunk.js"
+  },
+  {
+    "revision": "ebec6f5e9ed0ce98f5e4",
+    "url": "/static/js/6.fa493c37.chunk.js"
+  },
+  {
+    "revision": "af488a48f0afe042268a",
+    "url": "/static/js/7.75136a3c.chunk.js"
+  },
+  {
+    "revision": "d6ba8d2639611fbd0b95",
+    "url": "/static/js/8.bc564972.chunk.js"
+  },
+  {
+    "revision": "02c604ee35796fa4eb23",
+    "url": "/static/js/main.3fb66476.chunk.js"
+  },
+  {
+    "revision": "7a4d1c67dd91990934a5",
+    "url": "/static/js/runtime-main.3801e6a8.js"
+  }
+]);
